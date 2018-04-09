5c8649135c0b3f102b48fd3d3f515fce6755205a
Justin M. Rigler <justinmrigler@gmail.com>
2018-04-09 16:49:31 -0400

    completed first two tasks of part 2

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ export default class App extends Component {
         <Route exact path="/" component={MovieList} />
         <Route path="/movies/id" component={Movie} />
         </div>
+        <MovieDetails />
       </div>
     );
   }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Movies from './Movie.js';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -34,6 +36,7 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    <Link to='/movies/${movie.id}'>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -50,5 +53,6 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    </Link>
   );
 }
